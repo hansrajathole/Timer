721cca81f89037c253c255d6b991c0fb35fd45e6
hansrajathole <hansrajathole1@gmail.com>
2025-05-30 00:30:51 +0530

    Initialize React Timer App with Vite
    - Created package.json with dependencies for React, Vite, and ESLint.
    - Added Vite logo SVG to public directory.
    - Created App component to display current time, updating every second.
    - Added React logo SVG to assets directory.
    - Created CSS files for styling the application.
    - Set up main entry point for React application in main.jsx.
    - Configured Vite with react plugin in vite.config.js.

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  let date = new Date()
+  const [currentTime, setCurrentTime] = useState(date);
+
+
+  useEffect(() => {
+    setInterval(()=>{
+        setCurrentTime(new Date())
+    },1000)
+    return () => {
+      clearInterval()
+    }
+  }, [])
+  
+  return (
+      <p>Current Time: {currentTime.toLocaleTimeString()}</p>
+  );
+}
+
+export default App;
