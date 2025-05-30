1bb327f2e508c2fe0b014c0a5b06e5e5cb227e3f
hansrajathole <hansrajathole1@gmail.com>
2025-05-30 14:07:53 +0530

    Refactor App component and enhance CSS for better layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, use } from 'react';
 
 function App() {
   let date = new Date()
   const [currentTime, setCurrentTime] = useState(date);
-
-
+  const [todo, settodo] = useState([])
+  const [taks, settaks] = useState('')
   useEffect(() => {
     setInterval(()=>{
         setCurrentTime(new Date())
@@ -13,12 +13,36 @@ function App() {
       clearInterval()
     }
   }, [])
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    let time = currentTime.toLocaleTimeString()
+    let obj = {
+      time : time,
+      taks : taks
+    }
+
+    settodo((prev)=> [...prev , obj])
+
+  }
   
   return (
     <>
       
       <p>Current Time: {currentTime.toLocaleTimeString()}</p>
-      
+      <form onSubmit={handleSubmit} >
+        <input type="text" value={taks} onChange={(e)=>settaks(e.target.value)}/>
+        <button>Add</button>
+      </form>
+      <div>
+        {
+          todo.map((list , idx)=> <div className='list'>
+            <div>{list.taks}</div>
+            <p>{list.time}</p>
+          </div>)
+        }
+      </div>
+
     </>
   );
 }
